a9c08c49ab5157feb977882895786ae5faf29e76
Juliana-placido <juliana.lplacido@icloud.com>
2023-05-03 23:51:08 -0300

    Update TemaRepository.java

--- src/main/java/com/generation/repository/TemaRepository.java
+++ src/main/java/com/generation/repository/TemaRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 @Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
-    List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+    List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao, IgnoreCase);
 }
+
